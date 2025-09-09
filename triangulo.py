18a991d458b565d0a2e20c35979f0b84202c9d26
lucas-pinheiro-costa <cadastro.lpc@gmail.com>
2025-09-09 15:12:18 -0300

    primeiro commit

---
+++ triangulo.py
@@ -0,0 +1,8 @@
+int l1, l2, l3 = None, None, None
+
+print("\n=== CLASSIFICAÇÃO DE TRIÂNGULOS ===\n")
+print("\n\tInstrução 01: insira um valor inteiro para o primeiro lado do triângulo: ")
+
+print("\n\tInstrução 02: insira um valor inteiro para o segundo lado do triângulo: ")
+
+print("\n\tInstrução 03: insira um valor inteiro para o terceiro lado do triângulo: ")
